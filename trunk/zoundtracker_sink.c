9da289a1c4d43b424dbdedd08c4c627850b350ce
mvm9289 <mvm9289@adbfb499-0977-ec55-e4ac-4cb938e59061>
2010-11-12 14:06:08 +0000

    Added CFS in Zoundtracker*.c

--- trunk/zoundtracker_sink.c
+++ trunk/zoundtracker_sink.c
@@ -2,42 +2,107 @@
 #include "contiki.h"
 #include "net/rime.h"
 #include "net/rime/mesh.h"
-#include "dev/leds.h" /******************/
 
 #include <stdio.h>
+#include <string.h>
 
 #define MY_ADDR1 111
 #define MY_ADDR2 111
 #define CHANNEL 111
 
-static struct mesh_conn zoundtracker_conn;
+#define NODE_TABLE_SIZE 200
+#define POLL_INTERVAL CLOCK_SECOND*30
+#define MAX_RESENDS 5
 
 
 
 PROCESS(zoundtracker_sink_process, "ZoundTracker Sink Process");
 AUTOSTART_PROCESSES(&zoundtracker_sink_process);
 
+//---------------------------------------------------------------------------------------------------------------------
+
+typedef struct
+{
+	unsigned short addr1;
+	unsigned short addr2;
+	unsigned char empty;
+} NodeTableEntry;
+
+static NodeTableEntry nodeTable[NODE_TABLE_SIZE];
 
 
-static void sent(struct mesh_conn *c) {}
+static struct mesh_conn zoundtracker_conn;
 
-static void timedout(struct mesh_conn *c) {}
+
+static unsigned char isReceived;
+
+static int i;
+
+static int resends;
+
+//---------------------------------------------------------------------------------------------------------------------
+
+static void sent(struct mesh_conn *c)
+{
+	printf("Packet sent.\n\n");
+}
+
+static void timedout(struct mesh_conn *c)
+{
+	printf("Packet timeouted. Resends = %d\n\n", resends+1);
+	if (resends++ < MAX_RESENDS)
+	{
+		rimeaddr_t addr_send;
+		addr_send.u8[0] = nodeTable[i].addr1;
+		addr_send.u8[1] = nodeTable[i].addr2;
+		packetbuf_copyfrom("poll", 4);
+		
+		mesh_send(&zoundtracker_conn, &addr_send);
+	}
+	else isReceived = 1;
+}
 
 static void received(struct mesh_conn *c, const rimeaddr_t *from, uint8_t hops)
 {
-	printf("Packet received:\n From: %d.%d\n Data length: %d\n Data: %.*s\n Hops: %d\n",
-		from->u8[0], from->u8[1], packetbuf_datalen(),
-		packetbuf_datalen(), (char *)packetbuf_dataptr(), hops);
+	if (!strcmp((char *)packetbuf_dataptr(), "hello"))
+	{
+		int j;
+		unsigned char exit = 0;
+		for (j = 0; j < NODE_TABLE_SIZE && !exit; j++)
+		{
+			if (nodeTable[j].empty)
+			{
+				nodeTable[j].empty = 0;
+				nodeTable[j].addr1 = from->u8[0];
+				nodeTable[j].addr2 = from->u8[1];
+				exit = 1;
+			}
+		}
+		if (!exit) printf("NODE TABLE COMPLETE!\n\n");
+	}
+	else
+	{
+		printf("Packet received:\n From: %d.%d\n Data length: %d\n Data: %.*s\n Hops: %d\n\n",
+			from->u8[0], from->u8[1], packetbuf_datalen(),
+			packetbuf_datalen(), (char *)packetbuf_dataptr(), hops);
+		isReceived = 1;
+	}
 }
 
 const static struct mesh_callbacks zoundtracker_sink_callbacks = {received, sent, timedout};
 
-
+//---------------------------------------------------------------------------------------------------------------------
 
 PROCESS_THREAD(zoundtracker_sink_process, ev, data)
 {
 	PROCESS_EXITHANDLER(mesh_close(&zoundtracker_conn);)
 	PROCESS_BEGIN();
+	
+	int j;
+	for (j = 0; j < NODE_TABLE_SIZE; j++)
+	{
+		nodeTable[j].empty = 1;
+	}
 	
 	rimeaddr_t my_addr;
 	my_addr.u8[0] = MY_ADDR1;
@@ -47,17 +112,34 @@ PROCESS_THREAD(zoundtracker_sink_process, ev, data)
 	mesh_open(&zoundtracker_conn, CHANNEL, &zoundtracker_sink_callbacks);
 	
 	static struct etimer timer;
-	etimer_set(&timer, CLOCK_SECOND);
-	
-	leds_init(); /******************/
+	etimer_set(&timer, POLL_INTERVAL);
 
 	while (1)
 	{
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-		leds_green(LEDS_ON); /******************/
-		etimer_reset(&timer);
-		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-		leds_green(LEDS_OFF); /******************/
+		for (i = 0; i < NODE_TABLE_SIZE; i++)
+		{
+			if (!nodeTable[i].empty)
+			{
+				rimeaddr_t addr_send;
+				addr_send.u8[0] = nodeTable[i].addr1;
+				addr_send.u8[1] = nodeTable[i].addr2;
+				packetbuf_copyfrom("poll", 4);
+				
+				isReceived = 0;
+				resends = 0;
+				printf("Sending packet\n\n");
+				mesh_send(&zoundtracker_conn, &addr_send);
+				
+				static struct etimer timer2;
+				etimer_set(&timer2, CLOCK_SECOND);
+				//while (!isReceived)
+				//{
+					PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer2));//Si no llega, quitarla de la tabla
+					//etimer_reset(&timer2);
+				//}
+			}
+		}
 		etimer_reset(&timer);
 	}
 	PROCESS_END();
