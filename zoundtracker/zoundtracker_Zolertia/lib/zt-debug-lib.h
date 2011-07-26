b10610b75a7028b1dd564b20ae7a7877f874fcc3
beac89 <beac89@adbfb499-0977-ec55-e4ac-4cb938e59061>
2011-07-26 11:01:26 +0000

    librería para mensajes de debug (no está completa)

---
+++ zoundtracker/zoundtracker_Zolertia/lib/zt-debug-lib.h
@@ -0,0 +1,31 @@
+#ifndef __ZT_DEBUG_LIB_H__
+#define __ZT_DEBUG_LIB_H__
+
+//NET messages
+void debug_debug_net_sending_message(char* message);
+void debug_net_packet_content(Packet *packet);
+void debug_net_sending_WORKING_FILE(int packet_number);
+void debug_net_timeout(int type,int packet_number);
+void debug_net_max_attempts();
+void debug_net_message_lost(int type, int packet_number);
+void debug_net_message_received_connection(char* connection);
+void debug_net_message_received(char* message);
+void debug_net_incorrect_checksum();
+void debug_net_info_net(int packets_sent, int packets_acknowledged);
+
+//STATE messages
+void debug_state_current_state(char *state);
+
+//FILEMAN messages
+//TODO
+
+//EVENT messages
+void debug_event_timer_expired();
+
+//SENSOR messages
+void debug_sensor_sample_measured(int sample_number);
+
+
+
+#endif /*__ZT_DEBUG_LIB_H__*/
+
